b5ebaa0d2cd3e5eac257c9c98cfe5f0b896b4335
ssjtroly <ssjtroly@gmail.com>
2018-09-15 13:17:27 -0500

    Add files via upload

--- js/randomizer/enemies/randomizeKoopaKidHP.js
+++ js/randomizer/enemies/randomizeKoopaKidHP.js
@@ -1,6 +1,6 @@
 function randomizeKoopaKidHP(ROM) {
 	// patch must be applied for koopalingHPPointer to be functional
-	jsps.applyObjectInline(patch.koopalingHPPatch, ROM);
+	jsps.applyObjectInline(patch.koopalingHP, ROM);
 
 	ui.addSpoiler("Randomized Koopaling HP");
 	for (var i = 0; i < 8; i++) {
