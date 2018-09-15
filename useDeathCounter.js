c5d5406830673220d335f3d4bd80dd03f714440c
ssjtroly <ssjtroly@gmail.com>
2018-09-15 13:15:56 -0500

    Delete useDeathCounter.js

--- useDeathCounter.js
+++
@@ -1,9 +0,0 @@
-function setUseDeathCounter(ROM) {
-	jsps.applyObjectInline(patch.disable1Ups, ROM);
-	jsps.applyObjectInline(patch.useDeathCounter, ROM);
-
-	ROM[rom.startingLivesPointer] = 0;
-	ROM[rom.continueLivesPointer] = 0;
-	ui.addSpoiler("Death Counter Enabled");
-	ui.addSpoiler();
-}
