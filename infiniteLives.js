16c95173ad5d879b1d2f695895cc9759706ab103
ssjtroly <ssjtroly@gmail.com>
2018-09-15 13:13:38 -0500

    oops

--- infiniteLives.js
+++
@@ -1,9 +0,0 @@
-function setInfiniteLives(ROM) {
-	jsps.applyObjectInline(patch.disable1Ups, ROM);
-	jsps.applyObjectInline(patch.infiniteLives, ROM);
-
-	ROM[rom.startingLivesPointer] = rom.livesQuestionMarkValue;
-	ROM[rom.continueLivesPointer] = rom.livesQuestionMarkValue;
-	ui.addSpoiler("Infinite Lives Enabled");
-	ui.addSpoiler();
-}
